9828e2025ed7e39059d6425252fc7b50fe7e9cac
Orxan477 <tu201906147@code.edu.az>
2023-08-13 19:11:05 +0400

    initial commit

---
+++ File/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = "E:\\Music\\Orxan"; // Dosyaların bulunduğu dizin yolu
+            string targetText = "spotifydown.com - "; // Silmek istediğiniz metin
+
+            try
+            {
+                string[] files = Directory.GetFiles(directoryPath, "*.mp3");
+
+                foreach (string filePath in files)
+                {
+                    string fileName = Path.GetFileName(filePath);
+                    string newFileName = fileName.Replace(targetText, "");
+
+                    string newFilePath = Path.Combine(directoryPath, newFileName);
+                    System.IO.File.Move(filePath, newFilePath);
+
+                    Console.WriteLine($"Dosya adı değiştirildi: {fileName} -> {newFileName}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata oluştu: " + ex.Message);
+            }
+        }
+    }
+}
